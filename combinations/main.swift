6905fcad41dd1308b5378d3819ff8e5a463030fa
Michael Welch <michaelgwelch@gmail.com>
2014-10-29 14:02:56 -0500

    Initial Commit

---
+++ combinations/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  combinations
+//
+//  Created by Michael Welch on 10/29/14.
+//  Copyright (c) 2014 Michael Welch. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
